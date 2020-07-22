9fb5d10d8a03996de112f5c48f49d86a00479510
Kevin van der Velden <KevinVDVelden@users.noreply.github.com>
2020-07-22 11:40:44 +0000

    Implemented discovery and setting the state topic. State topic is determined by lopping off '/set' and adding '/state'

--- mqtt.py
+++ mqtt.py
@@ -1,6 +1,8 @@
 import paho.mqtt.client as mqtt
 import homeeasy
 import os
+import time
+import json
 
 class MqttPersistence:
     def __init__( self, prefix, client ):
@@ -13,17 +15,55 @@ class MqttPersistence:
             self.prefix += '/'
 
         self.data = {}
+        self.topic_mapping = {}
 
-    def set_homeeasy( self, homeeasy ):
+    def set_homeeasy( self, homeeasy, discovery_prefix ):
         self.homeeasy = homeeasy
+        if discovery_prefix[-1] != '/':
+            discovery_prefix += '/'
+
+        self.discovery_prefix = discovery_prefix
 
     def on_connect( self, client, userdata, flags, rc ):
         assert( client == self.client )
         print( 'Connected and subscribing to "%s".' % ( self.prefix, ) )
         client.subscribe( self.prefix + '#' )
+        print( 'Done' )
+
+        if self.discovery_prefix != '':
+            print( 'Sending discovery information' )
+            data = {
+                    "availability_topic": self.discovery_prefix + "homeeasy",
+                    "payload_on": "on",
+                    "payload_off": "off",
+                    "state_on": "on",
+                    "state_off": "off",
+                    "retain": "true",
+
+                    "payload_available": "online",
+                    "payload_not_available": "offline",
+                    }
+
+            for switch in self.homeeasy.switches:
+                #self.client.publish( self.discovery_prefix + 'light'
+                key = self.discovery_prefix + 'switch/homeeasy/' + 'homeeasy_switch_' + str( switch.id ) + '/config'
+                data["command_topic"] = switch_topic = self.prefix + 'homeeasy_switch_' + str( switch.id ) + '/set'
+                data["state_topic"] = switch_topic = self.prefix + 'homeeasy_switch_' + str( switch.id ) + '/state'
+                data["unique_id"] = "homeeasy_dev_" + str( switch.id )
+                data["name"] = switch.name
+                self.client.publish( key, payload = json.dumps( data ), retain = True )
+                self.topic_mapping[ 'homeeasy_switch_' + str( switch.id ) ] = switch.id
+
+            self.client.publish( self.discovery_prefix + 'homeeasy', 'online', retain = True )
+            self.client.will_set( self.discovery_prefix + 'homeeasy', 'offline', retain = True )
+
+    def disconnect( self ):
+        print( 'Shutting down' )
+        self.client.publish( self.discovery_prefix + 'homeeasy', 'offline', retain = True )
+        time.sleep( 0.5 )
+        self.client.disconnect()
 
     def on_message( self, client, userdata, msg ):
-        print( msg.topic+" "+str(msg.payload) )
         if not msg.topic.startswith( self.prefix ):
             return
 
@@ -43,12 +83,20 @@ class MqttPersistence:
         
         topic = msg.topic[ len( self.prefix ): ].split( '/' )
         if len( topic ) == 2:
+            he_key = topic[0]
+            if he_key in self.topic_mapping:
+                he_key = self.topic_mapping[he_key]
+
             if topic[1] == 'set':
-                self.homeeasy.set_state( 'switch', topic[0], value, None )
+                self.client.publish( self.prefix + topic[0] + '/state', ( 'on' if value else 'off' ), retain = True )
+                self.homeeasy.set_state( 'switch', he_key, value, None )
+            elif topic[1] in ( 'config', 'state' ):
+                pass
             else:
                 print( 'Unknown topic: ' + '/'.join( topic ) )
         elif len( topic ) == 1:
-            self.data[ topic[ 0 ] ] = value
+            if topic[0] in self.topic_mapping:
+                self.data[ self.topic_mapping[ topic[ 0 ] ] ] = value
         else:
             print( 'Unknown topic: ' + '/'.join( topic ) )
 
@@ -72,7 +120,9 @@ class MqttPersistence:
 
 
 MQTT_BROKER                 = os.getenv( 'MQTT_BROKER',            'localhost' )
-MQTT_PREFIX                 = os.getenv( 'MQTT_PREFIX',            'ha/switch/homeeasy' )
+MQTT_PREFIX                 = os.getenv( 'MQTT_PREFIX',            'homeassistant/switch/homeeasy' )
+
+MQTT_HA_DISCOVERY           = os.getenv( 'MQTT_HA_DISCOVERY',      '' )
 
 HOMEEASY_ADDRESS            = os.getenv( 'HOMEEASY_ADDRESS',       'homeeasy.lan' )
 HOMEEASY_USER               = os.getenv( 'HOMEEASY_USER',          'admin' )
@@ -81,8 +131,11 @@ HOMEEASY_PASSWORD_HASH      = os.getenv( 'HOMEEASY_PASSWORD_HASH', '96e79218965e
 client = mqtt.Client()
 persistence = MqttPersistence( MQTT_PREFIX, client )
 ha = homeeasy.HomeEasy( HOMEEASY_ADDRESS, HOMEEASY_USER, HOMEEASY_PASSWORD_HASH, persistence )
-persistence.set_homeeasy( ha )
+persistence.set_homeeasy( ha, MQTT_HA_DISCOVERY )
 
 client.connect( MQTT_BROKER, 1883, 60 )
 
-client.loop_forever()
+try:
+    client.loop_forever()
+except KeyboardInterrupt:
+    persistence.disconnect()
